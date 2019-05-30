7f2ca4341796b2d2555a41725ed3d855967e7e3e
Michał Martyniak <michal.martyniak@linux.pl>
2019-05-30 13:06:47 +0200

    Initial Commit

---
+++ GithubClient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GithubClient
+//
+//  Created by Michał Martyniak on 05/30/2019.
+//  Copyright © 2019 Michał Martyniak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GithubClientTests/GithubClientTests.swift
@@ -0,0 +1,34 @@
+//
+//  GithubClientTests.swift
+//  GithubClientTests
+//
+//  Created by Michał Martyniak on 05/30/2019.
+//  Copyright © 2019 Michał Martyniak. All rights reserved.
+//
+
+import XCTest
+@testable import GithubClient
+
+class GithubClientTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
